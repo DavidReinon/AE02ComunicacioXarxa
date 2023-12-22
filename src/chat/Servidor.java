6eca391101d1c9f3032c6843eac2eb712a85ac74
DavidReinon <davidreinongarcia@gmail.com>
2023-12-22 19:58:50 +0100

    funcion autenticacio en el servidor

--- src/chat/Servidor.java
+++ src/chat/Servidor.java
@@ -1,10 +1,35 @@
 package chat;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Servidor {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+	}
+
+	private static boolean AutenticacioUsuari(String usuario, String Contrasenya) {
+		File autenticacio = new File("autenticacio.txt");
+		boolean resultat = false;
+		try {
+			BufferedReader br = new BufferedReader(new FileReader(autenticacio));
+			String linea;
+			String[] usuariInfo = new String[2];
+			while ((linea = br.readLine()) != null) {
+				usuariInfo = linea.split(";");
+				if (usuariInfo[0].equals(usuario) && usuariInfo[1].equals(Contrasenya)) {
+					resultat = true;
+				}
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return resultat;
 	}
 
 }
