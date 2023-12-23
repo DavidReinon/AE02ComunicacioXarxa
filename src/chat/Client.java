85389994f9528ca6c33fbb36d7bb7ef7a3f5c2c8
DavidReinon <davidreinongarcia@gmail.com>
2023-12-24 00:55:47 +0100

    cambis alejandro: conexio del client amb autenticacio

--- src/chat/Client.java
+++ src/chat/Client.java
@@ -1,9 +1,33 @@
 package chat;
 
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
+
 public class Client {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		try {
+			Socket connexio = new Socket("localhost", 1234);
+
+			OutputStream os = connexio.getOutputStream();
+			PrintWriter pw = new PrintWriter(os);
+			Scanner teclat = new Scanner(System.in);
+			// while() {
+			System.out.print("Usuario: ");
+			String usuario = teclat.nextLine();
+			System.out.print("Contrasenya: ");
+			String contrasenya = teclat.nextLine();
+			pw.write(usuario + "\n");
+			pw.write(contrasenya + "\n");
+			pw.flush();
+			System.out.println("Missatge enviat");
+			// }
+			teclat.close();
+		} catch (Exception e) {
+			System.out.println(e);
+		}
 
 	}
 
--- src/chat/Servidor.java
+++ src/chat/Servidor.java
@@ -2,14 +2,44 @@ package chat;
 
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.io.FileReader;
 
 public class Servidor {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		try {
+			ServerSocket socketEscolta = new ServerSocket(1234);
+			Socket connexio = socketEscolta.accept();
+			InputStream is = connexio.getInputStream();
+			InputStreamReader isr = new InputStreamReader(is);
+			BufferedReader br = new BufferedReader(isr);
+			String missatge = br.readLine();
+			System.out.println(missatge);
+			BufferedReader br2 = new BufferedReader(isr);
+			String contrasenya = br.readLine();
+			System.out.println(contrasenya);
+			if (AutenticacioUsuari(missatge, contrasenya)) {
+				System.out.println("Usuari y contrasenya correctes");
+			} else {
+				System.out.println("Usuari o contrasenya incorrectes");
+
+			}
+			OutputStream os = connexio.getOutputStream();
+			PrintWriter pw = new PrintWriter(os);
+			pw.write(missatge.toString() + "\n");
+			pw.flush();
+			System.err.println("SERVIDOR >>> Espera nova peticio");
+		} catch (Exception e) {
+			System.out.println(e);
+
+		}
 
 	}
 
